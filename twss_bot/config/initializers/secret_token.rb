f2b11ad2d1474bba4ad9fd17db6accfc6dcf53e1
AzharB <azhar.bandeali@gmail.com>
2012-12-11 21:11:12 -0500

    New Rails Project
    Hope this works

---
+++ twss_bot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwssBot::Application.config.secret_token = 'c07f5fbc63517ff78a34576b886be95a0e56eb13e97384c1173ebc2e067f1fe7f9f5a2207c77bedc44660b481c31b77b925b1e049dbf90e6bfb770b980d8cfba'
